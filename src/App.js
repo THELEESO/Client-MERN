469cf5e6a1954549c1ccec4798328da2f727e2d2
Leeso <leeso@LeesodeMacBook-Air.local>
2022-09-04 23:49:20 +0800

    front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import HomeComponent from "./components/HomeComponent";
+import NavComponent from "./components/NavComponent";
+import RegisterComponent from "./components/RegisterComponent";
+import LoginComponent from "./components/LoginComponent";
+import ProfileComponent from "./components/ProfileComponent";
+import CourseComponent from "./components/CourseComponent";
+import PostCourseComponent from "./components/PostCourseComponent";
+import AuthService from "./services/auth.service";
 
 function App() {
+  let [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavComponent currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      <Switch>
+        <Route path="/" exact>
+          <HomeComponent />
+        </Route>
+        <Route path="/register" exact>
+          <RegisterComponent />
+        </Route>
+        <Route path="/login" exact>
+          <LoginComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />
+        </Route>
+        <Route path="/profile" exact>
+          <ProfileComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />
+        </Route>
+        <Route path="/course" exact>
+          <CourseComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />
+        </Route>
+        <Route path="/postcourse" exact>
+          <PostCourseComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/services/auth.service.js
@@ -0,0 +1,25 @@
+// 提供一個服務，嫁接前端與後端
+import axios from "axios";
+const API_URL = "http://localhost:8080/api/user";
+
+class AuthService {
+  login(email, password) {
+    return axios.post(API_URL + "/login", { email, password });
+  }
+  logout() {
+    localStorage.removeItem("user");
+  }
+  register(username, email, password, role) {
+    return axios.post(API_URL + "/register", {
+      username,
+      email,
+      password,
+      role,
+    });
+  }
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthService();
